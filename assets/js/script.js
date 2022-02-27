cb7787815e36a9914704d0397afbed38fc2723bf
Jez <jezielandres@gmail.com>
2022-02-27 15:27:21 -0300

    Se avanzo en el header, hero y Hobby: programacion

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* 
+let change_img = (() => {
+    let hero_img = document.querySelector(".hero__img img");
+    let src_list = [
+        "assets/img/152_kiro.webp",
+        "assets/img/1.webp",
+        "assets/img/Jez (wolf angel).webp",
+        "assets/img/navidad.webp"
+    ];
+
+    src_list.forEach((src, index) => {
+        setTimeout(() => {
+            hero_img.src = src;
+        }, (index * 30000));
+    });
+    
+})(); */

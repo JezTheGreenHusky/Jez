7cb0f622761aaa98714f59b60ba4e6ccf0bd94e6
Jez <jezielandres@gmail.com>
2022-02-27 22:35:54 -0300

    http -> https

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,8 +192,8 @@ STEAM
 
 // https://developer.valvesoftware.com/wiki/Steam_Web_API#Game_interfaces_and_methods
 
-let resumen_jugador = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamids=76561199041830855"
-let ultimos_juegos = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamid=76561199041830855&format=json";
+let resumen_jugador = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamids=76561199041830855"
+let ultimos_juegos = "https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamid=76561199041830855&format=json";
 
 let module2 = (async() => {
     let request = await fetch(resumen_jugador);

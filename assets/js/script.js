28f82269aa4383c2e685e04b7123106c0a431b04
Jez <jezielandres@gmail.com>
2022-12-05 00:07:52 -0300

    el proxy se ah cambiado porque heroku ya no es gratis qwp

--- assets/js/script.js
+++ assets/js/script.js
@@ -214,8 +214,8 @@ window.addEventListener("load", ()=> {
     
     // https://developer.valvesoftware.com/wiki/Steam_Web_API#Game_interfaces_and_methods
     
-    let resumen_jugador = "https://cors-proxy.htmldriven.com/?url=https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamids=76561199041830855"
-    let ultimos_juegos = "https://cors-proxy.htmldriven.com/?url=https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamid=76561199041830855&format=json";
+    let resumen_jugador = "https://api.allorigins.win/raw?url=http://my.api.com/get/stuffhttps://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamids=76561199041830855"
+    let ultimos_juegos = "https://api.allorigins.win/raw?url=http://my.api.com/get/stuffhttps://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=7823223CAD5CD4EB1F21E8E4A8069BAB&steamid=76561199041830855&format=json";
     
     let module_steam = (async() => {
         let request = await fetch(resumen_jugador);
